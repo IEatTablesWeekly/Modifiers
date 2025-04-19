2fc489f326d23383ec0dea7924343d02f762b887
IEatTablesWeekly <ieattablesweekly@gmail.com>
2025-04-19 21:34:14 +0530

    Ability modification
    Removed "X-ray"
    Modified Voidhandle to just make mining faster
    Changed colors of all the abilities

--- BP/scripts/custom/pickaxe/pickaxe.js
+++ BP/scripts/custom/pickaxe/pickaxe.js
@@ -1,4 +1,4 @@
-// import "./ability";
+import "./ability";
 import { toAllPlayers, processInventoryWithModifiers } from "../../utils/utils";
 
 const pickaxeKeywords = [
@@ -8,32 +8,31 @@ const pickaxeKeywords = [
 
 const ABILITIES = {
     common: [
-        { name: "Sturdy", description: "- [§a+§7§o] 20% chance to not consume durability.", color: "§r§a" },
-        { name: "Collector", description: "- [§a+§7§o] You do not lose durability while mining ores.", color: "§r§a" },
-        { name: "Exhaustion", description: "- [§c-§7§o] You randomly get exhausted, causing you to be enable to mine.", color: "§r§3" },
+        { name: "Sturdy", description: "- [§a+§7§o] 20% chance to not consume durability.", color: "§r§7" },
+        { name: "Collector", description: "- [§a+§7§o] You do not lose durability while mining ores.", color: "§r§7" },
+        { name: "Exhaustion", description: "- [§c-§7§o] You randomly get exhausted, causing you to be enable to mine.", color: "§r§7" },
     ],
     uncommon: [
-      { name: "Prospector", description: "- [§a+§7§o] 10% chance to double common ore drops.", color: "§r§6" },
-      { name: "Infested", description: "- [§c-§7§o] 5% chance to spawn silverfish when mining stone.", color: "§r§8" },
-      { name: "Jeweller", description: "- [§c+§7§o] Mining an ore will give you haste for 10 seconds.", color: "§r§8" },
+      { name: "Prospector", description: "- [§a+§7§o] 10% chance to double common ore drops.", color: "§r§g" },
+      { name: "Infested", description: "- [§c-§7§o] 5% chance to spawn silverfish when mining stone.", color: "§r§q" },
+      { name: "Jeweller", description: "- [§c+§7§o] Mining an ore will give you haste for 10 seconds.", color: "§r§5" },
     ],
     rare: [
-        { name: "Earthshaker", description: "- [§a+§7§o] Mining damages and pushes back nearby enemies.", color: "§r§6" },
-        { name: "Vein Miner", description: "- [§c-§7§o] Mining a single ore from a vein will break the entire vein.", color: "§r§3" },
-        { name: "Glass Edge", description: "- [§c-§7§o] You mine 5x faster but consume 5x durability.", color: "§r§3" }
+        { name: "Earthshaker", description: "- [§a+§7§o] Mining damages and pushes back nearby enemies.", color: "§r§n" },
+        { name: "Vein Miner", description: "- [§a+§7§o] Mining a single ore from a vein will break the entire vein.", color: "§r§s" },
+        { name: "Glass Edge", description: "- [§c-§7§o] You mine 5x faster but consume 5x durability.", color: "§r§r" }
       ],
     epic: [
         { name: "Magnetized", description: "- [§a+§7§o] Automatically pulls mined ores near you.", color: "§r§b" },
-        { name: "Seismic", description: "- [§a+§7§o] Mines in a 3x3 radius.", color: "§r§b" },
         { name: "Volcanic", description: "- [§a+§7§o] Smelts ores directly when mined.", color: "§r§4" },
         { name: "Unbreakable", description: "- [§a+§7§o] This item is unbreakable and will never break.", color: "§r§9" },
         { name: "Claustrophobic", description: "- [§c-§7§o] Mining in tight spaces will make you dizzy.", color: "§r§8" },
     ],
     legendary: [
-      { name: "Midas Touch", description: "- [§a+§7§o] 25% of getting random ores from mining stone.", color: "§r§6" },
-      { name: "X-ray", description: "- [§a+§7§o] The location of nearby ores will be displayed on your screen.", color: "§r§9" },
-      { name: "Voidhandle", description: "- [§a+§7§o] You break hard blocks like obsidian, reinforced deepslate and bedrock with ease.", color: "§r§9" },
-      { name: "Marble Touch", description: "- [§a+§7§o] Blocks you break will not transform into another form upon dropping", color: "§r§9" },
+      { name: "Midas Touch", description: "- [§a+§7§o] 25% of getting random ores from mining stone.", color: "§r§e" },
+      { name: "Voidhandle", description: "- [§a+§7§o] You can break blocks instantly.", color: "§r§8" },
+      { name: "Ceramic Touch", description: "- [§a+§7§o] Blocks you break will not transform into another form upon dropping", color: "§r§m" },
+      { name: "Seismic", description: "- [§a+§7§o] Mines in a 3x3 radius.", color: "§r§n" },
     ]
 };
 
