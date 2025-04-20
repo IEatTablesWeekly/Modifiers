1ab58f97b93509ae0332d43e8535488ab776e346
IEatTablesWeekly <ieattablesweekly@gmail.com>
2025-04-20 22:11:18 +0530

    Delete voidhandle.js

--- BP/scripts/custom/pickaxe/legendary/voidhandle.js
+++
@@ -1,9 +0,0 @@
-import { hasLoreInHeldItem , toAllPlayers } from '../../../utils/utils.js';
-import { TicksPerSecond } from '@minecraft/server'
-
-function tryApplyVoidHandle(player) {
-    if (!hasLoreInHeldItem(player, 'voidhandle')) return;
-    player.addEffect("haste", TicksPerSecond * 2, { amplifier: 2, showParticles: false });
-}
-
-toAllPlayers(tryApplyVoidHandle,5);
