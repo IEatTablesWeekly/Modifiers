09c9b2cd7f2d53fb937d093412dc0c9c8c2bd162
IEatTablesWeekly <ieattablesweekly@gmail.com>
2025-04-20 22:11:46 +0530

    Update lucky.js

--- BP/scripts/custom/sword/uncommon/lucky.js
+++ BP/scripts/custom/sword/uncommon/lucky.js
@@ -6,9 +6,9 @@ const lootTable = [
   { id: 'minecraft:iron_nugget', weight: 30, min: 1, max: 7, color: 'f' },
   { id: 'minecraft:gold_nugget', weight: 15, min: 1, max: 6, color: '6' },
   { id: 'minecraft:iron_ingot', weight: 5, min: 1, max: 2, color: 'f' },
-  { id: 'minecraft:gold_nugget', weight: 5, min: 1, max: 2, color: '6' },
+  { id: 'minecraft:gold_ingot', weight: 5, min: 1, max: 2, color: '6' },
   { id: 'minecraft:emerald', weight: 4, min: 1, max: 1, color: 'a' },
-  { id: 'minecraft:diamond', weight: 1, min: 1, max: 1, color: 'b' },
+  { id: 'minecraft:diamond', weight: 0.5, min: 1, max: 1, color: 'b' },
   { id: 'minecraft:netherite_scrap', weight: 0.5, min: 1, max: 1, color: 'n' },
 ];
 
@@ -42,7 +42,7 @@ world.afterEvents.entityHurt.subscribe((event) => {
     return str.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
   }
   system.runTimeout(() => {
-    if (deadEntity.isValid() && Math.random() < 0.4) {
+    if (deadEntity.isValid() && Math.random() < 0.1) {
       const pos = deadEntity.location;
       const drop = getWeightedRandomItem();
       if (drop) {
