ef767085ae71c4beba530fecca6a8d4d40d88a9a
IEatTablesWeekly <ieattablesweekly@gmail.com>
2025-04-21 22:46:41 +0530

    Create earthshaker.js

---
+++ BP/scripts/custom/pickaxe/rare/earthshaker.js
@@ -0,0 +1,68 @@
+// doesn't work well cureently il fix later (trust me bro)
+import { world , TicksPerSecond } from '@minecraft/server';
+import { hasLoreInHeldItem } from '../../../utils/utils.js';
+
+const excludeEntities = new Set([
+  "minecraft:item","agent", "area_effect_cloud", "armor_stand", "arrow", "boat",
+  "chest", "chest_boat", "chest_minecart", "command_block_minecart",
+  "dragon_fireball", "minecart", "fireball", "egg", "ender_crystal",
+  "ender_pearl", "eye_of_ender_signal", "fireworks_rocket", "fishing_hook",
+  "hopper_minecart", "lightning_bolt", "lingering_potion", "player", "potion",
+  "llama_spit", "npc", "shulker_bullet", "snowball", "small_fireball",
+  "splash_potion", "thrown_trident", "tnt", "tnt_minecart", "tripod_camera",
+  "wither_skull", "wither_skull_dangerous", "xp_bottle", "xp_orb"
+]);
+
+function spawnParticleRing(dimension, center, radius, count = 20) {
+    for (let i = 0; i < count; i++) {
+        const angle = (i / count) * Math.PI * 2;
+        const x = center.x + radius * Math.cos(angle);
+        const z = center.z + radius * Math.sin(angle);
+        const y = center.y + 1;
+
+        dimension.runCommandAsync(`particle minecraft:sculk_charge_pop_particle ${x} ${y} ${z}`);
+    }
+}
+
+function tryApplyEarthshaker(player){
+    if (!hasLoreInHeldItem(player, "earthshaker")) return;
+    if (player.isSneaking) return;
+
+    player.onScreenDisplay.setActionBar('§r[§nEarth Shaker§r]');
+    // player.playSound('beacon.activate');
+
+    const dimension = player.dimension;
+    const origin = player.location;
+    const radius = 9;
+
+    spawnParticleRing(dimension, origin, radius);
+
+    const nearbyEntities = dimension.getEntities({
+        location: origin,
+        maxDistance: radius - 1,
+        excludeEntities: Array.from(excludeEntities)
+    });
+
+
+    for (const entity of nearbyEntities) {
+        if (entity.id === player.id) continue;
+
+        const directionX = entity.location.x - origin.x;
+        const directionZ = entity.location.z - origin.z;
+        const length = Math.sqrt(directionX ** 2 + directionZ ** 2);
+
+        if (length === 0) continue;
+
+        const knockX = directionX / length;
+        const knockZ = directionZ / length;
+
+        entity.applyKnockback(knockX, knockZ, 1, 0.7);
+        entity.applyDamage(3, { cause: "thorns", damagingEntity: player });
+    }
+}
+
+world.afterEvents.playerBreakBlock.subscribe(({ player }) => {
+    if (player?.isValid()) {
+        tryApplyEarthshaker(player);
+    }
+});
