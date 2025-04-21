df447326449f1ef1281b3ae1348f729f6ee8c0c5
IEatTablesWeekly <ieattablesweekly@gmail.com>
2025-04-21 22:46:22 +0530

    Create claustrophobic.js

---
+++ BP/scripts/custom/pickaxe/epic/claustrophobic.js
@@ -0,0 +1,31 @@
+import { world, TicksPerSecond } from '@minecraft/server';
+import { hasLoreInHeldItem } from '../../../utils/utils.js';
+
+function tryApplyClaustrophobic(player) {
+    if (!hasLoreInHeldItem(player, 'claustrophobic')) return;
+
+    const headBlockLoc = {
+        x: Math.floor(player.location.x),
+        y: Math.floor(player.location.y + 2),
+        z: Math.floor(player.location.z)
+    };
+
+    const aboveBlock = player.dimension.getBlock(headBlockLoc);
+
+    if (aboveBlock?.typeId !== 'minecraft:air') {
+        player.addEffect("darkness", TicksPerSecond * 2, { amplifier: 0, showParticles: false });
+        player.addEffect("weakness", TicksPerSecond * 2, { amplifier: 0, showParticles: false });
+        if (Math.random() < 0.05) {
+            player.addEffect("blindness", TicksPerSecond * 7, { amplifier: 0, showParticles: false });
+            player.addEffect("nausea", TicksPerSecond * 7, { amplifier: 0, showParticles: false });
+        }
+        player.playSound('ambient.cave');
+        player.onScreenDisplay.setActionBar('§r[§8Claustrophobic§r]');
+    }
+}
+
+world.afterEvents.playerBreakBlock.subscribe(({ player }) => {
+    if (player?.isValid()) {
+        tryApplyClaustrophobic(player);
+    }
+});
